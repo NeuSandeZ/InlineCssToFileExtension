620f68a9e52475b99a20e364fb8fb6420dcc6aa9
Wojciech Massalski <smileyu@interia.pl>
2024-04-07 11:23:17 +0200

    extracting to a file works

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,129 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import {
+  ExtensionContext,
+  Range,
+  TextDocument,
+  commands,
+  languages,
+  window,
+  workspace,
+} from "vscode";
+import { ContextualActionProvider } from "./code-action-provider";
+import path from "path";
+import * as fs from "fs";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
+  let registerCodeActionsProvider = languages.registerCodeActionsProvider(
+    { scheme: "file", language: "html" },
+    new ContextualActionProvider()
+  );
+  context.subscriptions.push(registerCodeActionsProvider);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "inlinecsstofile" is now active!');
+  let moveToNewFileCommand = commands.registerCommand(
+    "extension.extractInlineCssToNewFile",
+    moveToNewFile
+  );
+  context.subscriptions.push(moveToNewFileCommand);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('inlinecsstofile.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from InlineCssToFile!');
-	});
-
-	context.subscriptions.push(disposable);
+  let moveToExistingFileCommand = commands.registerCommand(
+    "extension.extractInlineCssToFile",
+    moveToExistingFile
+  );
+  context.subscriptions.push(moveToExistingFileCommand);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
+
+async function moveToNewFile(
+  document: TextDocument,
+  inlineCss: string,
+  pair: number[]
+) {
+  const fileName =
+    (await window.showInputBox({
+      prompt: "Enter file name",
+    })) + ".css";
+
+  const className = await window.showInputBox({
+    prompt: "Enter class name",
+  });
+
+  if (!className || !fileName) {
+    await window.showErrorMessage("Name cannot be empty");
+    return;
+  }
+
+  //TODO give it a thought about opening file
+  // const newUri = vscode.Uri.file(cssFilePath);
+  // vscode.workspace.openTextDocument(newUri).then((doc) => {
+  //   vscode.window.showTextDocument(doc);
+  // });
+
+  const cssFilePath = path.join(
+    workspace.workspaceFolders![0].uri.fsPath,
+    fileName
+  );
+
+  window.activeTextEditor?.edit((editBuilder) => {
+    if (pair[3]) {
+      const positionToInsert = document.positionAt(pair[3] - 1);
+      editBuilder.delete(
+        new Range(
+          document.positionAt(pair[3] + 1),
+          document.positionAt(pair[1])
+        )
+      );
+      editBuilder.insert(positionToInsert, " " + className);
+    } else {
+      editBuilder.replace(
+        new Range(document.positionAt(pair[0]), document.positionAt(pair[1])),
+        `class="${className}"`
+      );
+    }
+  });
+
+  const extractedStyle = inlineCss.split('"')[1];
+  fs.writeFileSync(cssFilePath, `.${className} { ${extractedStyle} }`);
+}
+
+async function moveToExistingFile(
+  document: TextDocument,
+  inlineCss: string,
+  pair: number[]
+) {
+  const cssFiles = await workspace.findFiles("**/*.css");
+  if (cssFiles.length === 0) {
+    window.showErrorMessage("No .css file was found!");
+    //TODO redirect to creating?
+    return;
+  }
+
+  const cssFileNames = cssFiles.map((file) => path.basename(file.path));
+  const chosenFile = await window.showQuickPick(cssFileNames);
+
+  if (!chosenFile) {
+    return;
+  }
+
+  const className = await window.showInputBox({
+    prompt: "Enter class name",
+  });
+
+  if (!className) {
+    return;
+  }
+
+  const cssFilePath = path.join(
+    workspace.workspaceFolders![0].uri.fsPath,
+    chosenFile
+  );
+
+  window.activeTextEditor?.edit((editBuilder) => {
+    editBuilder.replace(
+      new Range(document.positionAt(pair[0]), document.positionAt(pair[1])),
+      `class="${className}"`
+    );
+  });
+
+  const extractedStyle = inlineCss.split('"')[1];
+  fs.appendFileSync(cssFilePath, `\n\n.${className} { ${extractedStyle} }`);
+}
