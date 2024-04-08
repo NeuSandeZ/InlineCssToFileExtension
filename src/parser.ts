44631912037b4bd81426b27c4ea590fc67a48c2d
Wojciech Massalski <smileyu@interia.pl>
2024-04-08 22:58:56 +0200

    removed unnecessery code

--- src/parser.ts
+++ src/parser.ts
@@ -4,13 +4,8 @@ export async function ParseText(document: string): Promise<number[][]> {
   let indexes: number[][] = [];
   let indexesOfClass: number[] = [];
   let indexesOfStyle: number[] = [];
-  let indexOfOpenTag: number;
-  let indexOfCloseTag: number;
 
   const parser = new Parser({
-    onopentag(name) {
-      indexOfOpenTag = parser.startIndex;
-    },
     onattribute(name, value) {
       if (name === "class") {
         indexesOfClass.push(parser.startIndex, parser.endIndex);
@@ -20,10 +15,8 @@ export async function ParseText(document: string): Promise<number[][]> {
       }
     },
     onclosetag(name) {
-      indexOfCloseTag = parser.startIndex;
-
       if (indexesOfClass.length > 0 && indexesOfStyle.length > 0) {
-        indexes.push([indexesOfStyle[0], indexesOfStyle[1], indexesOfClass[1]]);
+        indexes.push([...indexesOfStyle, ...indexesOfClass]);
       } else if (indexesOfStyle.length > 0) {
         indexes.push([...indexesOfStyle]);
       }
