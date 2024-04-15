517880d7d168aa4e559f543fd0fdbe06299bfcf8
Wojciech Massalski <smileyu@interia.pl>
2024-04-15 22:04:15 +0200

    rewrote indexer cause there was issue with the previous one, it indexed uncorrectly when nesting occured, renamed parser.ts to indexer.ts

---
+++ src/indexer.ts
@@ -0,0 +1,46 @@
+import { Parser } from "htmlparser2";
+
+export interface IndexesResult {
+  indexesForCommands: number[][];
+  lastLinkIndex: number | null;
+}
+
+export async function GetIndexes(document: string): Promise<IndexesResult> {
+  const indexesForCommands: number[][] = [];
+  let indexesOfClass: number[] = [];
+  let indexesOfStyle: number[] = [];
+  let lastLinkIndex: number | null = null;
+
+  const parser = new Parser({
+    onopentag(name, attribs) {
+      if (attribs.style && attribs.class) {
+        indexesForCommands.push([...indexesOfStyle, ...indexesOfClass]);
+        indexesOfStyle = [];
+        indexesOfClass = [];
+      } else if (attribs.style) {
+        indexesForCommands.push([...indexesOfStyle]);
+        indexesOfStyle = [];
+      } else if (attribs.class) {
+        indexesOfClass = [];
+      }
+    },
+    onattribute(name, value) {
+      console.log("in attribute", name);
+      if (name === "style" && value.trim() !== "") {
+        indexesOfStyle.push(parser.startIndex, parser.endIndex);
+      } else if (name === "class") {
+        indexesOfClass.push(parser.startIndex, parser.endIndex);
+      }
+    },
+    onclosetag(name, isImplied) {
+      if (name === "link" && isImplied) {
+        lastLinkIndex = parser.endIndex;
+      }
+    },
+  });
+
+  parser.write(document);
+  parser.end();
+
+  return { indexesForCommands, lastLinkIndex };
+}
